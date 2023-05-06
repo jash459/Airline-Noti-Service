3efceb431ba1fe3c917230421acf5ef33424f4de
Jash <jashhinger62@gamil.com>
2023-05-06 21:25:57 +0530

    Added basic node express functionlities

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const { ServerConfig, Logger } = require('./config')
+const apiRoutes = require('./routes')
+const logger = require('./config/logger-config')
+
+const app = express()
+
+app.use('/api', apiRoutes)
+
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`)
+})
