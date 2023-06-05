7367e71b7df0338211a37b90f2bd8fadc5099e47
Jash <jashhinger62@gamil.com>
2023-06-05 20:38:27 +0530

    Created nodemailer tarnsport object and tried

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,5 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports={
-    PORT :process.env.PORT
+    PORT :process.env.PORT,
+    GMAIL_PASS : process.env.GAMIL_PASS,
+    GAMIL_EMAIL:process.env.GAMIL_EMAIL,
 }
