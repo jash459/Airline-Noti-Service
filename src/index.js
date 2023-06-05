c4ef1b21d4e782cb2930071b9517ccba87be9f85
Jash <jashhinger62@gamil.com>
2023-06-05 21:26:31 +0530

    Added create ticket api

--- src/index.js
+++ src/index.js
@@ -14,14 +14,4 @@ app.use('/api', apiRoutes)
 
 app.listen(ServerConfig.PORT, async() => {
   console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`)
-  try {
-    const response = await mailsender.sendMail({
-      from: ServerConfig.GAMIL_EMAIL,
-      to:'abc@gmail.com',
-      subject:'Is the service working?',
-      text: 'Yes it is working'
-    })
-  } catch (error) {
-    console.log(error)
-  }
 })
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require("express");
 
-const { InfoController } = require('../../controllers')
+const { InfoController, EmailController } = require("../../controllers");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/info', InfoController.info)
+router.get("/info", InfoController.info);
 
-module.exports = router
+router.post("/tickets", EmailController.create);
+
+module.exports = router;
